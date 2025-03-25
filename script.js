abc8fc6d2f467c27f3a558c1194fb6915b289a54
rashirawal22 <rashi.rawal@kalvium.community>
2025-03-26 01:11:50 +0530

    Donr

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Function to get the value of a cookie by name
+document.addEventListener("DOMContentLoaded", function () {
   function getCookie(name) {
-    let cookieArray = document.cookie.split('; ');
-    let cookie = cookieArray.find((row) => row.startsWith(name + '='));
-    return cookie ? cookie.split('=')[1] : null;
+      let cookieArray = document.cookie.split("; ");
+      let cookie = cookieArray.find((row) => row.startsWith(name + "="));
+      return cookie ? cookie.split("=")[1] : null;
   }
 
-  // Function to set a cookie
   function setCookie(name, value, daysToExpire) {
-    let date = new Date();
-    date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
-    document.cookie =
-      name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+      let date = new Date();
+      date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
+      document.cookie = name + "=" + value + ";expires=" + date.toUTCString() + ";path=/";
   }
 
-  // 1. Get the value of the 'count' cookie
-  // 2. If the cookie exists, increment the value and update the cookie
-  // 3. If the cookie does not exist, create it and set the value to 1
-  // 4. Display the count on the webpage
+  function incrementCount() {
+      let count = getCookie("count"); 
+      if (count) {
+          count = parseInt(count) + 1;
+      } else {
+          count = 1; 
+      }
+      setCookie("count", count, 7); 
+      document.getElementById("count").innerText = count;
+  }
 
-  // your code here
+  incrementCount(); 
 });
